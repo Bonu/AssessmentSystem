01edcf0e7da413b94a98f72d14cde8cd32eb9318
Michael <michaelfayez@gmail.com>
2015-06-17 12:04:30 -0500

    add docs for uservalidator

--- src/edu/mum/cs/waa/fp/as/domain/User.java
+++ src/edu/mum/cs/waa/fp/as/domain/User.java
@@ -1,14 +1,13 @@
 package edu.mum.cs.waa.fp.as.domain;
 
-import java.util.List;
-import java.util.Set;
+ 
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
- 
+import javax.persistence.Transient;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -63,7 +62,7 @@ public class User {
 	public void setVerifyPassword(String verifyPassword) {
 		this.verifyPassword = verifyPassword;
 	}
-	
+	@Transient
 	private String verifyPassword;
 
 	public int getUserId() {
